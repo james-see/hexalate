da701e01cd6ab5c065b3f75dbd22be8feb89d42f
James Campbell <james@jamescampbell.us>
2026-09-03 14:47:38 -0400

    fix: add long_description from README for PyPI, bump to 0.2.1

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name='hexalate',
-    version='0.2.0',
+    version='0.2.1',
     description='A package for generating hexagonal tessellations',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='James Campbell',
     author_email='james@jamescampbell.us',
+    url='https://github.com/james-see/hexalate',
     packages=find_packages(),
     install_requires=[
         'matplotlib',
@@ -13,7 +19,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'hexalate = hexalate.__main__:main',  # Change this line
+            'hexalate = hexalate.__main__:main',
         ],
     },
 )
